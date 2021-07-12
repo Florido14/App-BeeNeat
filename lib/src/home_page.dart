1b33b93aada392b5ba9bf717e88989f28c8ae404
Florido14 <51244465+Florido14@users.noreply.github.com>
2021-07-11 22:43:08 -0500

    pantalla proyectos hecha

--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:beeneatapp/src/mainproyectos.dart';
 import 'package:beeneatapp/src/newproyectos.dart';
 import 'package:beeneatapp/src/proyectos.dart';
 import 'package:flutter/material.dart';
@@ -23,7 +24,6 @@ class HomePage extends StatelessWidget {
                     child: ListView(
                       children: <Widget>[
                         miCard(context),
-                        miCardDesign(context),
                       ],
                     ),
                   )
@@ -44,35 +44,9 @@ class HomePage extends StatelessWidget {
               onTap: () {
                 Navigator.of(context).pushAndRemoveUntil(
                   MaterialPageRoute(
-                    builder: (context) => ExistProyect(),
+                    builder: (context) => mainproyecto(),
                   ),
                   ModalRoute.withName('/ExistProyect'),
-                );
-              },
-            ),
-            height: 200,
-            fit: BoxFit.cover,
-          )
-        ],
-      ),
-    );
-  }
-
-  Card miCardDesign(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      elevation: 20,
-      child: Stack(
-        children: [
-          Ink.image(
-            image: AssetImage('assets/images/Proyectos_B.png'),
-            child: InkWell(
-              onTap: () {
-                Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(
-                    builder: (context) => NewProyecto(),
-                  ),
-                  ModalRoute.withName('/newProyecto'),
                 );
               },
             ),
